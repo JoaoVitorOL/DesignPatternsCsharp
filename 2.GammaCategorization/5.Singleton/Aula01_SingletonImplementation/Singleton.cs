e6c3aadba3048ba96a5d1d21749416792327c3d1
JoaoVitorOL <ghostcode541@gmail.com>
2026-07-09 14:06:56 -0300

    SIngleton Lazy

--- 2.GammaCategorization/5.Singleton/Aula01_SingletonImplementation/Singleton.cs
+++ 2.GammaCategorization/5.Singleton/Aula01_SingletonImplementation/Singleton.cs
@@ -5,17 +5,31 @@ using Microsoft.VisualBasic;
 namespace Aula01_SingletonImplementation
 {
 
+    // ===== Interface =====
+    // Contrato que representa o "acesso a dados de populacao".
     public interface IDatabase
     {
         int GetPopulation(string name);
     }
 
 
+    // ===== Classe =====
+    // Implementa o pattern Singleton: garante que exista, no maximo,
+    // UMA instancia de `SingletonDatabase
     public class SingletonDatabase : IDatabase
     {
+
+        
         private Dictionary<string, int> capitals;
 
-        public SingletonDatabase()
+
+        // ===== Construtores =====
+        // Le o arquivo "capitals.txt" e monta o dicionario de capitais.
+        // Este construtor só deve ser chamado uma vez na aplicação
+        // o modificador de acesso "private" garante isso, fazendo com que
+        // não tenha como chamar ele externamente usando "new" para criar uma nova instância .
+
+        private SingletonDatabase() // Construtor privado. Assim, não tem como chamar ele externamente com new para criar uma nova instância .
         {
             Console.WriteLine("Initializing Database . . .");
 
@@ -33,10 +47,17 @@ namespace Aula01_SingletonImplementation
         }
 
         
-
+    // privtae static SingletonDatabase instance = new SingletonDatabase();
+    //                                                                 Inicializa o Singleton Forma tradicional
+    // public static SingletonDatabase Instance => instance;
+    //                                                                 Expõe Singleton  Forma tradicional
         private static Lazy<SingletonDatabase> instance = 
-          new Lazy<SingletonDatabase>(() => new SingletonDatabase());
-        public static SingletonDatabase Instance => instance.Value;
+          new Lazy<SingletonDatabase>(() => new SingletonDatabase()); // Inicializa Singleton (Lazy)
+        public static SingletonDatabase Instance => instance.Value; // Expõe Singleton
+
+
+        //Apenas cria o Singleton do Databse quando alguém acessa a instância.
+        // Por que? Porque a lambda é ativada só quando se faz o get do .Value
 
 
     }
